2ee3f472eb24fc0c724004bd489a16aca0c8685a
Nguyen Van Tri <vantri1010@gmail.com>
2025-04-13 00:10:02 +0700

    33_amount_wallet_api, 32_amount_blockchain_api : Creating API that will let the blockchain server and wallet server return the cryptocurrency total

--- block/blockchain.go
+++ block/blockchain.go
@@ -267,3 +267,15 @@ func (tr *TransactionRequest) Validate() bool {
 	}
 	return true
 }
+
+type AmountResponse struct {
+	Amount float32 `json:"amount"`
+}
+
+func (ar *AmountResponse) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Amount float32 `json:"amount"`
+	}{
+		Amount: ar.Amount,
+	})
+}
